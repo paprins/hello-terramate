73f58ff0b89f0e70019a218673f1c2359aec2d6f
Pascal Prins <pascal.prins@foobar-it.com>
2024-12-19 15:15:36 +0100

    wip

--- stacks/a/__backend.tf
+++ stacks/a/__backend.tf
@@ -6,6 +6,5 @@ terraform {
     key                  = "terraform/stacks/by-id/f13566c1-dcba-4879-b4f4-f6a19a5a0230/terraform.tfstate"
     resource_group_name  = "rg-terraform"
     storage_account_name = "stterraform4201ca9cd363"
-    use_oidc             = true
   }
 }
